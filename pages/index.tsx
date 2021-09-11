b5b3b45c7b70a957b91b2f8fdcc693bbf495562c
ralmayer <llev4n@gmail.com>
2021-09-11 12:30:37 +0400

    removing getServerSideProps has helped to improve performance. trying to see if getStaticProps is any better

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Nav from "../components/Nav";
 import Inventory from "../components/Inventory";
 import Recipes from "../components/Recipes";
 
-const Home: NextPage<any> = () => {
+const Home: NextPage<any> = ({ data }) => {
   return (
     <div className="App max-w-screen bg-zeldabg xl:max-h-screen min-h-screen xl:h-screen flex flex-col divide-y divide-def divide-solid overflow-x-hidden overflow-hidden">
       <header className="relative px-6 py-6 md:px-16 md:py-12 bg-black bg-opacity-75 backdrop-filter backdrop-blur-lg flex justify-between">
@@ -17,6 +17,9 @@ const Home: NextPage<any> = () => {
       </header>
       <main className="flex-1 flex flex-wrap xl:flex-nowrap xl:px-4 py-10 xl:py-4 gap-10 xl:gap-20 justify-center content-center items-center max-w-full bg-black bg-opacity-30 backdrop-filter backdrop-blur">
         <Inventory key={1} />
+        {data && (
+          <Recipes data={{ potions: data.potions, dishes: data.dishes }} />
+        )}
       </main>
       <footer className="p-4 bg-black bg-opacity-75 backdrop-filter backdrop-blur-lg">
         footer
@@ -25,5 +28,20 @@ const Home: NextPage<any> = () => {
   );
 };
 
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const refPotions = await prisma.referencePotion.findMany({
+    include: { recipe: true },
+  });
+  const refDishes = await prisma.referenceDish.findMany({
+    include: { recipe: true },
+  });
+  const data = {
+    potions: refPotions,
+    dishes: refDishes,
+  };
+  return {
+    props: { data },
+  };
+};
 
 export default Home;
